203d2ed7527186c8d91e37d7f77fc3cce06bf0d6
kichikuou <KichikuouChrome@gmail.com>
2025-01-01 10:41:56 +0900

    decompile.bat: Add confirmation prompt when sys3c.cfg exists

--- misc/decompile.bat
+++ misc/decompile.bat
@@ -1,4 +1,15 @@
 @echo off
+setlocal
+
+if not exist src/sys3c.cfg goto decompile
+set /p answer="src/sys3c.cfg already exists. Do you want to continue? (y/n): "
+if /i not "%answer%"=="y" (
+    echo Operation aborted.
+    pause
+    exit /b 1
+)
+
+:decompile
 sys3dc . --outdir=src
 if errorlevel 1 (
     echo.
